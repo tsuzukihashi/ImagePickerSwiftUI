cfc2cf68db93fef0d6158c9c17e1f94229ee9844
tsuzukihashi <tsudukihashi0817@gmail.com>
2021-12-20 22:49:07 +0900

    fix crop

--- Sources/ImagePickerSwiftUI/UIImageExtension.swift
+++ Sources/ImagePickerSwiftUI/UIImageExtension.swift
@@ -12,25 +12,8 @@ public extension UIImage {
                 height: resizeSize
             )
         )
+
         guard let cropCGImage = cropCGImage else { return nil }
-        let cropImage = UIImage(cgImage: cropCGImage)
-        return cropImage.rotate(angle: 90)
-    }
-
-    func rotate(angle: CGFloat) -> UIImage? {
-        UIGraphicsBeginImageContextWithOptions(.init(width: size.width, height: size.height), false, 0)
-
-        guard let context: CGContext = UIGraphicsGetCurrentContext(), let cgImage = cgImage else { return nil }
-        context.translateBy(x: size.width / 2, y: size.height / 2)
-        context.scaleBy(x: 1.0, y: -1.0)
-
-        let radian: CGFloat = (-angle) * CGFloat.pi / 180.0
-
-        context.rotate(by: radian)
-        context.draw(cgImage, in: .init(x: -size.width / 2, y: -size.height / 2, width: size.width, height: size.height))
-
-        guard let rotatedImage = UIGraphicsGetImageFromCurrentImageContext() else { return nil }
-        UIGraphicsEndImageContext()
-        return rotatedImage
+        return UIImage(cgImage: cropCGImage, scale: 0, orientation: imageOrientation)
     }
 }
