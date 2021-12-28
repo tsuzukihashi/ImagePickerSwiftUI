8c62d8ff636d5b2dc2228391c7eb0a04f3897330
tsuzukihashi <tsudukihashi0817@gmail.com>
2021-12-28 15:07:37 +0900

    fix crop

--- Sources/ImagePickerSwiftUI/UIImageExtension.swift
+++ Sources/ImagePickerSwiftUI/UIImageExtension.swift
@@ -4,7 +4,7 @@ public extension UIImage {
     func croppingToSquare() -> UIImage? {
         guard let cgImage = cgImage else { return nil }
         let resizeSize = min(cgImage.width, cgImage.height)
-        let cropCGImage = cgImage.cropping(
+        return cropping(
             to: .init(
                 x: (cgImage.width - resizeSize) / 2,
                 y: (cgImage.height - resizeSize) / 2,
@@ -12,8 +12,31 @@ public extension UIImage {
                 height: resizeSize
             )
         )
+    }
 
-        guard let cropCGImage = cropCGImage else { return nil }
-        return UIImage(cgImage: cropCGImage, scale: 0, orientation: imageOrientation)
+    func cropping(to rect: CGRect) -> UIImage? {
+        let croppingRect: CGRect = imageOrientation.isLandscape ? rect.switched : rect
+        guard let cgImage: CGImage = self.cgImage?.cropping(to: croppingRect) else { return nil }
+        let cropped: UIImage = UIImage(cgImage: cgImage, scale: scale, orientation: imageOrientation)
+        return cropped
+    }
+}
+
+extension CGRect {
+    var switched: CGRect {
+        return CGRect(x: minY, y: minX, width: height, height: width)
+    }
+}
+
+extension UIImage.Orientation {
+    var isLandscape: Bool {
+        switch self {
+        case .up, .down, .upMirrored, .downMirrored:
+            return false
+        case .left, .right, .leftMirrored, .rightMirrored:
+            return true
+        @unknown default:
+            fatalError()
+        }
     }
 }
